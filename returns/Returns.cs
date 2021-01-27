54a819fbbd019905a16bf95c4a08d6a6e8a40636
Pawel Lukasik <pawel.lukasik@makeitright.ia>
2021-01-27 10:28:37 +0100

    remove unused imports

--- returns/Returns.cs
+++ returns/Returns.cs
@@ -1,10 +1,5 @@
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using Newtonsoft.Json.Linq;
 using NUnit.Framework;
-using OpenQA.Selenium;
 using System;
-using System.Dynamic;
 
 namespace example_client
 {
