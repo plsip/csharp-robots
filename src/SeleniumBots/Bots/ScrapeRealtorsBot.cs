16c4354a4fe07198ea23d3f85b67dcc7bc1ac445
Pawel Lukasik <pawel.lukasik@makeitright.ia>
2021-02-05 17:40:02 +0100

    +5

--- src/SeleniumBots/Bots/ScrapeRealtorsBot.cs
+++ src/SeleniumBots/Bots/ScrapeRealtorsBot.cs
@@ -36,7 +36,7 @@ namespace SeleniumBots.Bots
         [Test]
         public void GetRealtors2()
         {
-            Console.WriteLine("Getting realtors +2");
+            Console.WriteLine("Getting realtors ++");
             Assert.True(true);
         }
     }
